345857403e5326a0d8d3126a3c95ca21499bd826
reichert621 <reichertjalex@gmail.com>
2024-02-29 14:22:54 -0500

    Initial Commit

---
+++ NewYorkTransit/NewYorkTransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewYorkTransitApp.swift
+//  NewYorkTransit
+//
+//  Created by Alex Reichert on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NewYorkTransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
